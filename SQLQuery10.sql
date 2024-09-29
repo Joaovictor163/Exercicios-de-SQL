0dd82da10db333bf264a5aa0c035255de9c0d0ba
joao_victor <jvictormf163@gmail.com>
2024-09-29 16:47:04 -0300

    adicionei um novo exercicio

---
+++ SQLQuery10.sql
@@ -0,0 +1,7 @@
+SELECT
+	ProductKey AS 'ID',
+	ProductName AS 'Produto'
+FROM
+	DimProduct
+WHERE BrandName = 'Contoso' AND ColorName = 'Silver' AND UnitPrice BETWEEN 10 AND 30
+ORDER BY UnitPrice DESC
